ce79dc028d45c503342417180b476cb94be9a7fd
Docile123 <imbereyemasoo@gmail.com>
2025-07-30 18:28:51 +0200

    feat:finalizing the kat

--- day-1/delete-occurences.js
+++ day-1/delete-occurences.js
@@ -1,8 +1,25 @@
 function deleteNth(arr,n){
   let count ={};
   for(let num of arr){
-    count[num] = (count)
+    count[num] = (count[num]||0)+1;
   }
+
+  const arrayCount =Object.fromEntries(Object.entries(count).map(([keys,value])=>{
+    return value >n ?[keys,n]:[keys,value]
+  })) ;
+
+  let final =[];
+
+  for(let keys in arrayCount){
+    if(arrayCount.hasOwnProperty(keys)){
+      let value = arrayCount[keys];
+      for(let i=0;i<value;i++){
+        final.push(Number(keys))
+      }
+    }
+  }
+  return final
 }
 
 console.log(deleteNth([1,1,3,3,7,2,2,2,2], 3))
+
