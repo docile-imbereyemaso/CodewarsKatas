fba6d651bd1f4d36d43094aed043f37482d68a06
Docile123 <imbereyemasoo@gmail.com>
2025-08-04 13:03:50 +0200

    feat: adding the logic to calculate the surface and volume of cube and cuboid

--- day-2/make-cuboid.js
+++ day-2/make-cuboid.js
@@ -16,9 +16,26 @@ class Cuboid {
 }
 class Cube extends Cuboid {
   // Don't forget to make a call to super ;)
+
+  constructor(length){
+    super(length);
+    this.width = length;
+    this.height = length;
+  }
+  get surfaceArea(){
+    return 6*(this.length*this.length);
+  }
+  get volume(){
+    return this.length*this.length*this.length
+  }
 }
 
 const cuboid = new Cuboid(1,2,3);
 console.log(cuboid);
 console.log(cuboid.surfaceArea);
 console.log(cuboid.volume);
+
+const cube = new Cube(1);
+console.log(cube);
+console.log(cube.surfaceArea);
+console.log(cube.volume);
