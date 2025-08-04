744a982e476a61c6e9ecf9b3035e46f5c81fc653
Docile123 <imbereyemasoo@gmail.com>
2025-08-04 12:59:08 +0200

    fix:change the logic

--- day-2/make-cuboid.js
+++ day-2/make-cuboid.js
@@ -2,21 +2,23 @@ class Cuboid {
   // TODO: Program Me
   constructor(length,width,height){
     this.length = length;
-    this.width =width;
-    this.height = height
+    this.width = width;
+    this.height = height;
   }
 
   get surfaceArea(){
-    return 6*(this.length*this.length)
+    return 2*(this.length*this.width+ this.length*this.height +this.height*this.width)
   }
+
   get volume(){
-    return this.length *this.length * this.length
+    return this.length *this.height*this.width
   }
 }
 class Cube extends Cuboid {
   // Don't forget to make a call to super ;)
 }
 
-const cube = new Cuboid(2,3,4);
-console.log(cube.surfaceArea);
-console.log(cube.volume);
+const cuboid = new Cuboid(1,2,3);
+console.log(cuboid);
+console.log(cuboid.surfaceArea);
+console.log(cuboid.volume);
