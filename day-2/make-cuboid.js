d2b196463fc5c2776c2f0e02b245c5048574fe2c
Docile123 <imbereyemasoo@gmail.com>
2025-08-04 12:43:33 +0200

    feat:Add more logic to the kata

--- day-2/make-cuboid.js
+++ day-2/make-cuboid.js
@@ -1,7 +1,22 @@
 class Cuboid {
   // TODO: Program Me
-  
+  constructor(length,width,height){
+    this.length = length;
+    this.width =width;
+    this.height = height
+  }
+
+  get surfaceArea(){
+    return 6*(this.length*this.length)
+  }
+  get volume(){
+    return this.length *this.length * this.length
+  }
 }
 class Cube extends Cuboid {
   // Don't forget to make a call to super ;)
 }
+
+const cube = new Cuboid(2,3,4);
+console.log(cube.surfaceArea);
+console.log(cube.volume);
