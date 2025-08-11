edcd69a2ef2022e9ac319a789296875863ddcc4a
Docile123 <imbereyemasoo@gmail.com>
2025-08-11 11:32:31 +0200

    feat:extraction codewar

---
+++ day-6/extractions.js
@@ -0,0 +1,20 @@
+const inputText = "Michael, how are you? - Cool, how is John Williamns and Michael Jordan? I don't know but Michael Johnson is fine. Michael do you still score points with LeBron James, Michael Green AKA Star and Michael Wood?";
+
+function getMichaelLastName(inputText) {
+  // your awesome code here
+  const textarray = inputText.split(" ");
+  let result =[];
+
+for(let i=0;i<textarray.length;i++){
+    if(textarray[i]==="Michael"){
+   if(!textarray[i+1].length<=2){
+    result.push(textarray[i+1])
+   }
+       
+    }
+} 
+return result.map(word=>{
+    return 
+})
+}
+console.log(getMichaelLastName(inputText))
