f44ab8e51300dd141c6c21cbafa86d9139b7ef25
Docile123 <imbereyemasoo@gmail.com>
2025-08-18 16:21:07 +0200

    fix:change find routes logic

--- day-11/follow-that-spy.js
+++ day-11/follow-that-spy.js
@@ -1,15 +1,23 @@
+const findRoutes = (routes)=>{
 
-function findRoutes(routes) {
- const set = new Set();
- let routesMap  = new Map(routes);
+const routesMap = new Map(routes);
 
- routesMap.forEach((value,key)=>{
-    set.add(key);
-    set.add(value);
- });
-
- return [...set];
+const starts = new Set(routesMap.keys());
+// finding the starting point
+for(let destination of routesMap.values()){
+    starts.delete(destination);
 }
+
+let start = [...starts][0];
+
+let result  =[start];
+
+while(routesMap.has(start)){
+    start = routesMap.get(start);
+    result.push(start);
+}
+return result.join(", ")
+}
+
+
 console.log(findRoutes([["MNL", "TAG"], ["CEB", "TAC"], ["TAG", "CEB"], ["TAC", "BOR"]]))
-
-
