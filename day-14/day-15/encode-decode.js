10c976774c0c56ffe2b378b79448ff4cad746180
Docile123 <imbereyemasoo@gmail.com>
2025-08-27 11:56:20 +0200

    feat: decode function

--- day-14/day-15/encode-decode.js
+++ day-14/day-15/encode-decode.js
@@ -17,11 +17,19 @@ function encode(str){
 
 function decode(str){
      let countMap = new Map ([[1,"a"],[2,"e"],[3,"i"],[4,"o"],[5,"u"]]);
-
+     let decoded = ""
      for(let i=0;i<str.length;i++){
-        console.log(str[i]);
+        const char = str[i];
+        const num = Number(char);
+
+        if(!isNaN(num) && countMap.has(num)){
+          decoded += countMap.get(num);    
+        }else{
+            decoded += char;
+        }
      }
      
+     return decoded;
 
 }
 
